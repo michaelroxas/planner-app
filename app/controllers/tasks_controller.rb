aa93aee54f9374937951aa5e2b4dcac317d91663
Michael Roxas <work.michaelroxas@gmail.com>
2021-10-09 13:40:19 +0800

    Config Tasks controller
    Route tasks data to `show` to-do list on each category

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,13 @@
 class TasksController < ApplicationController
   def create
-    @category = Category.find(params[:category_id])
-    @task = @category.tasks.create(task_params)
-    redirect_to category_path(@category)
+    @planner = Planner.find(params[:planner_id])
+    @task = @planner.tasks.create(task_params)
+    redirect_to planner_path(@planner)
   end
 
   private
-    def task_params
-      params.require(:task).permit(:task)
-    end
+  def task_params
+    params.require(:task).permit(:task)
+  end
+
 end
